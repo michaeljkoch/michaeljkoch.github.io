48631bab82f2922ed91284aa74dbc756ab2659d6
Michael Koch <michaeljaredkoch@gmail.com>
2023-10-01 15:32:13 -0400

    Updates

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const navClick = () => {
     const menu = document.querySelector('.menu-btn');
     const navbar = document.querySelector('.navbar');
     const nav = document.querySelector('.navbar .nav');
+    const content = document.querySelector('.content');
+    const footer = document.querySelector('.footer');
+    const menup = document.querySelector('.menu-btn p')
+    const close = document.querySelector('.close')
     // const links = document.querySelector('.navbar .nav .links');
     // const navred = document.querySelector('.navbar')
 
@@ -13,6 +17,31 @@ const navClick = () => {
     menu.addEventListener('click', () => {
         // navred.classList.toggle('nav-active');
         nav.classList.toggle('nav-active');
+    });
+
+    menu.addEventListener('click', () => {
+        // navred.classList.toggle('nav-active');
+        content.classList.toggle('content-off');
+    });
+
+    menu.addEventListener('click', () => {
+        // navred.classList.toggle('nav-active');
+        footer.classList.toggle('footer-off');
+    });
+
+    menu.addEventListener('click', () => {
+        // navred.classList.toggle('nav-active');
+        menup.classList.toggle('menu-btn-off');
+    });
+
+    menu.addEventListener('click', () => {
+        // navred.classList.toggle('nav-active');
+        menu.classList.toggle('close');
+    });
+
+    menu.addEventListener('click', () => {
+        // navred.classList.toggle('nav-active');
+        close.classList.toggle('show');
     });
 
     // menu.addEventListener('click', () => {
