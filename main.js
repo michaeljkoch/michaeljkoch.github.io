b89df0e1e9ab2e2607c69daddfaf9495f69babec
Michael Koch <michaeljaredkoch@gmail.com>
2023-10-04 20:57:59 -0400

    major changes to layout

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
-const navClick = () => {
-    const menu = document.querySelector('.menu-btn');
-    const navbar = document.querySelector('.navbar');
-    const nav = document.querySelector('.navbar .nav');
-    const content = document.querySelector('.content');
-    const footer = document.querySelector('.footer');
-    const menup = document.querySelector('.menu-btn p')
-    const close = document.querySelector('.close')
-    // const links = document.querySelector('.navbar .nav .links');
-    // const navred = document.querySelector('.navbar')
+const toggleButton = document.getElementsByClassName('menu-btn')[0]
+const navbarLinks = document.getElementsByClassName('nav-links')[0]
+const openBtn = document.getElementsByClassName('open')[0]
+const closeBtn = document.getElementsByClassName('close')[0]
 
-    menu.addEventListener('click', () => {
-        // navred.classList.toggle('nav-active');
-        navbar.classList.toggle('navbar-active');
-    });
-
-    menu.addEventListener('click', () => {
-        // navred.classList.toggle('nav-active');
-        nav.classList.toggle('nav-active');
-    });
-
-    menu.addEventListener('click', () => {
-        // navred.classList.toggle('nav-active');
-        content.classList.toggle('content-off');
-    });
-
-    menu.addEventListener('click', () => {
-        // navred.classList.toggle('nav-active');
-        footer.classList.toggle('footer-off');
-    });
-
-    menu.addEventListener('click', () => {
-        // navred.classList.toggle('nav-active');
-        menup.classList.toggle('menu-btn-off');
-    });
-
-    menu.addEventListener('click', () => {
-        // navred.classList.toggle('nav-active');
-        menu.classList.toggle('close');
-    });
-
-    menu.addEventListener('click', () => {
-        // navred.classList.toggle('nav-active');
-        close.classList.toggle('show');
-    });
-
-    // menu.addEventListener('click', () => {
-    //     // navred.classList.toggle('nav-active');
-    //     links.classList.toggle('links-active');
-    // });
-}
-
-navClick();
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+  openBtn.classList.toggle('active');
+  closeBtn.classList.toggle('active')
+})
