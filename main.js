18bf313dcda37af5d4b8893d4b62c3e76e128882
Michael Koch <michaeljaredkoch@gmail.com>
2025-05-16 19:35:41 -0400

    new design initial commit

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const toggleButton = document.getElementsByClassName('menu-btn')[0]
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
-const openBtn = document.getElementsByClassName('open')[0]
-const closeBtn = document.getElementsByClassName('close')[0]
+const toggleButton = document.getElementsByClassName('menu-btn')[0];
+const openBtn = document.getElementsByClassName('open')[0];
+const closeBtn = document.getElementsByClassName('close')[0];
+// const content = document.getElementsByClassName('content')[0];
+// const menuContent = document.getElementsByClassName('menu-content')[0];
+const nav = document.getElementsByTagName('nav')[0];
+const social = document.getElementsByClassName('social')[0];
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
   openBtn.classList.toggle('active');
-  closeBtn.classList.toggle('active')
+  closeBtn.classList.toggle('active');
+  // content.classList.toggle('active');
+  // menuContent.classList.toggle('active');
+  nav.classList.toggle('active');
+  social.classList.toggle('active');
 })
