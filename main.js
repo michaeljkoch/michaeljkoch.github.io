cf6e8c47472124e975de6399f17ca5ca5caabc04
Michael Koch <michaeljaredkoch@gmail.com>
2025-05-16 20:35:51 -0400

    fix mobile menu

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const toggleButton = document.getElementsByClassName('menu-btn')[0];
 const openBtn = document.getElementsByClassName('open')[0];
 const closeBtn = document.getElementsByClassName('close')[0];
-// const content = document.getElementsByClassName('content')[0];
-// const menuContent = document.getElementsByClassName('menu-content')[0];
+const body = document.getElementsByTagName('body')[0];
 const nav = document.getElementsByTagName('nav')[0];
 const social = document.getElementsByClassName('social')[0];
 
 toggleButton.addEventListener('click', () => {
   openBtn.classList.toggle('active');
   closeBtn.classList.toggle('active');
-  // content.classList.toggle('active');
-  // menuContent.classList.toggle('active');
+  body.classList.toggle('active');
   nav.classList.toggle('active');
   social.classList.toggle('active');
 })
